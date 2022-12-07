c98ff44db20c98ba3f69121a0c026133ed90ce79
Giannakius <giannakius10@gmail.com>
2022-12-07 19:51:26 +0200

    xwrisma grammwn done

--- leitoyrgika.cpp
+++ leitoyrgika.cpp
@@ -6,12 +6,10 @@
 using namespace std;
 int main()
 {
-
-
     int segmentation_degree = 10000; // Bathmos Katatmishs
-    segmentation_degree = segmentation_degree - 1;
+    segmentation_degree = segmentation_degree ;
 
-    ///////////////////////////
+    ////// Make The txt to string ////////////
     ifstream file("BigTest.txt", ios::binary);
     string fileStr;
 
@@ -21,13 +19,12 @@ int main()
     copy(inputIt, emptyInputIt, stringInsert);
 
     //cout << fileStr;
-    //////////////////////////
 
-    int string_size = fileStr.length();
+    //////////////////////////////////////////
+
+    int string_size = fileStr.length();     // Length of .txt file
     
-    int lines_count = 0 ;
-
-    // Count '\n' (change line) in BigText.txt
+    int lines_count = 0 ;                   // Count how many rows in .txt
     int x;
     lines_count = 0 ;
     for (x=0;x<string_size;x++){
@@ -35,31 +32,26 @@ int main()
             lines_count++;
         }
     }
-    //cout << count;
 
 
-    int chars_in_one_degree = lines_count / segmentation_degree ; 
-    //cout << chars_in_one_degree;
-
-
-    string array1[segmentation_degree];
+    string array1[segmentation_degree];         // Array with the segmended string in its rows
     int temp = 0 , k = 0 , start = 0 , counter =0;
 
-    while (counter<lines_count){
+    while (counter<=lines_count){
         while (k<=string_size and fileStr[k]!='\n' ){
-            k++;
+            k++;    // find all the chars until the new line
         }
-        array1[temp] = fileStr.substr(start,k);
+        array1[temp] = fileStr.substr(start,k);     // copy them to the array [temp]
         temp = temp + 1;
-        start = start + k +1;
+        start = start + k +1;   // fix the start for the new start
     //cout << k;
-    counter = counter + 1 ;
+    counter = counter + 1 ;     // lines counter to stop the while
     }
-    //cout << lines_count;
+
+    // Print the Array
     for (x=0; x<= lines_count ; x++){
         cout << array1[x] << '\n';
     }
-
 
     return 0;
 }
