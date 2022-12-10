b5e6a7fc522efb248e1ff28878368e3cc89d8345
Giannakius <giannakius10@gmail.com>
2022-12-11 00:46:47 +0200

    .

--- shared_memory.h
+++ shared_memory.h
@@ -1,10 +1,10 @@
 
+#define MAXSIZE 100
+
 struct shared_memory{
-    char *buffer;   // Content of the desired line
-    bool *in_use;  // Child completed or not (0 for not completed, 1 for completed)
+    int line;   // Desired line
+    char buffer[MAXSIZE];   // Content of the desired line
+    int completed;  // Child completed or not (0 for not completed, 1 for completed)
 };
 
 typedef struct shared_memory* sharedMemory;
-
-
-
