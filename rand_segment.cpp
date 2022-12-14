05ebc42283a35de7cd31256a16f8acfba6643abb
Giannakius <giannakius10@gmail.com>
2022-12-15 01:37:31 +0200

    trelos 2

--- rand_segment.cpp
+++ rand_segment.cpp
@@ -3,21 +3,22 @@
 
 using namespace std;
 
-int Rand_Segment(int segment)
+int Rand_Segment(int segment , int Num_Of_Segments)
 {
     int new_segment = segment;
     int PITHANOTHTA = 1 + (rand() % 10);    // pithanotita 1 - 10 
     
     // An tyxei 1-2-3-4-5-6-7 diladi 70% tote menoyme sto idio segment
     if (PITHANOTHTA<=7){
-        //cout << "Idio segment " ; 
+        cout << "Idio segment " ; 
     }
     else { // An tyxei 8-9-10 , diladi 30% pithanotita tote pame se opoiodipote segment ektos apo ayto poy eimastan
         while (new_segment == segment){
-            new_segment = 1 + (rand() % 100);
+            new_segment = 1 + (rand() % Num_Of_Segments);
         }
-        //cout << "allo segment " ; 
+        cout << "allo segment " ; 
     }
+    cout << new_segment << endl;
     return new_segment ;
 }
 
@@ -27,7 +28,7 @@ int main()
     int segment = 3 ;
     for(int i=0;i<60;i++)
     {
-        segment = Rand_Segment(segment);
+        segment = Rand_Segment(segment , 100);
     }
     return 0;
 }
