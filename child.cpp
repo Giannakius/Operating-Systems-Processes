ab13fb4c7da4fcebcd1a228e6a0262a8522850c4
Giannakius <giannakius10@gmail.com>
2022-12-14 20:54:19 +0200

    rand

--- child.cpp
+++ child.cpp
@@ -10,6 +10,24 @@
 
 #define SEM_PERMS (S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP)
 
+int Rand_Segment(int segment)
+{
+    int new_segment = segment;
+    int PITHANOTHTA = 1 + (rand() % 10);    // pithanotita 1 - 10 
+    
+    // An tyxei 1-2-3-4-5-6-7 diladi 70% tote menoyme sto idio segment
+    if (PITHANOTHTA<=7){
+        //cout << "Idio segment " ; 
+    }
+    else { // An tyxei 8-9-10 , diladi 30% pithanotita tote pame se opoiodipote segment ektos apo ayto poy eimastan
+        while (new_segment == segment){
+            new_segment = 1 + (rand() % 100);
+        }
+        //cout << "allo segment " ; 
+    }
+    return new_segment ;
+}
+
 void child(int nlines, int N, int i, Shared_Memory semlock, sem_t* requestProducer, sem_t* answerProducer, sem_t* answerConsumer){
 
     clock_t t;
